a7568bb72d8f3458adcd81b6572f047f2b2a7277
Adam Chomicki <admchm@outlook.com>
2019-06-09 09:07:23 +0200

    Add a poem to view

--- Contents.swift
+++ Contents.swift
@@ -1,10 +1,22 @@
 import SwiftUI
 import PlaygroundSupport
 
-struct LiveView: View {
+struct CustomContentView: View {
     var body: some View {
-        Text("SwiftUI in Mojave")
+        DetailView()
     }
 }
 
-PlaygroundPage.current.liveView = UIHostingController(rootView: LiveView())
+/* Poem */
+struct DetailView: View {
+    var body: some View {
+        VStack (alignment: .leading) {
+            Text("A Simple Plan").font(.headline)
+            Text("By Irwin Mercer").color(.gray).font(.system(size:14, design: .monospaced))
+            Text("Simple Sam was a simple man. He lived each day by a simple plan. Enjoy your life and live while you can. Make each day count and take a stand.").font(.body).lineLimit(nil)
+        }
+    }
+}
+
+// Setting the live view to a UIHostingController instance whose rootView conforms to View
+PlaygroundPage.current.liveView = UIHostingController(rootView: CustomContentView())
